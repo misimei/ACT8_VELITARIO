81777776c513cec07e9ce7a3c5b8eee7a2580268
misimei <velitariomissy@gmail.com>
2024-04-30 21:17:29 +0800

    add posts

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model
+class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','description','category','quantity','price','image'];
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.js', 'resources/js/app.js'])
+        <!-- Styles -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         @yield('content')
